2da0f8ed4990910fcdb272250120a7daa6fc48de
Jacob Krall <jacob@jkrall.net>
2012-01-28 17:00:21 -0500

    keyboard input: ASD / arrow keys

---
+++ scripts/input.js
@@ -0,0 +1,21 @@
+define(['jquery', 'controller'], function($, controller){
+	$(function(){
+		$('body').keydown(function(ev){
+			var keycode = ev.keyCode;
+			switch(keycode){
+				case 65: //A
+				case 37: //left arrow
+					controller.left();
+					break;
+				case 68: //D
+				case 39: // right arrow
+					controller.right();
+					break;
+				case 83: //S
+				case 40: // down arrow
+					controller.down();
+					break;
+			}
+		})
+	});
+});
